0918a349dc32c378675544809cd6b7b03dd00445
Sung Bae <singsang2@gmail.com>
2018-10-24 14:33:33 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,14 @@
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
-  # check 
-  arr.each do |word|
+  # check first two words from index 0 - n to determine which one is first using ALPHABET order - bring out the letter find the index from ALPHABET compare those indexes to determine which is first.
+  # if in order already, move on to second and third
+  arr_counter = 0
+  while arr_counter < arr.length - 1
+    alph_counter = 0
+      while  
+    arr[0]
+    arr[1]
+    
     
 end
